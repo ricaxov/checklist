531273ef96eec571cdf0e583402e82e6825a1c48
Ricardo Vieira <ricardovieiracdo@gmail.com>
2026-09-15 16:04:50 -0300

    adding stuff

--- checklist-app/src/App.tsx
+++ checklist-app/src/App.tsx
@@ -1,3 +1,39 @@
+import { useState } from 'react'
+import { TaskTable } from './components/TaskTable'
+import { mockTasks } from './data/mock'
+import { TaskForm } from './components/TaskForm'
 export function App() {
-  return <h1>oie</h1>
+  const [tasks, setTasks] = useState(mockTasks)
+
+  const activeTasks = tasks.filter((task) => task.completedAt === null)
+  const doneTasks = tasks.filter((task) => task.completedAt !== null)
+
+  function handleToggleDone(id: string) {
+    setTasks((currentTasks) =>
+      currentTasks.map((task) =>
+        task.id === id
+          ? {
+              ...task,
+              completedAt: task.completedAt ? null : new Date().toISOString(),
+            }
+          : task,
+      ),
+    )
+  }
+
+  return (
+    <div className="container py-4">
+      <TaskForm />
+      <TaskTable
+        heading="Active"
+        tasks={activeTasks}
+        onToggleDone={handleToggleDone}
+      />
+      <TaskTable
+        heading="Done"
+        tasks={doneTasks}
+        onToggleDone={handleToggleDone}
+      />
+    </div>
+  )
 }
